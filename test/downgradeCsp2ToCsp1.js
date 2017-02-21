2e888e6d516e3e350bca325a25d21531b7fcaa89
Andreas Lind <andreas@one.com>
2017-02-21 21:38:50 +0100

    Cover basic 'unsafe-inline' cases in downgradeCsp2ToCsp1, add tests.

---
+++ test/downgradeCsp2ToCsp1.js
@@ -0,0 +1,51 @@
+const expect = require('unexpected');
+const downgradeCsp2ToCsp1 = require('../lib/downgradeCsp2ToCsp1');
+const parseCsp = require('../lib/parseCsp');
+
+function normalizeParsedCsp(parsedCsp) {
+    const result = {};
+    Object.keys(parsedCsp).forEach(key => {
+        if (Array.isArray(parsedCsp[key])) {
+            result[key] = [].concat(parsedCsp[key]).sort();
+        } else if (parsedCsp[key]) {
+            result[key] = [ parsedCsp[key] ];
+        }
+    });
+    return result;
+}
+
+expect.addAssertion('<object|string> to come out as <object|string>', (expect, subject, value) => {
+    if (typeof subject === 'string') {
+        subject = parseCsp(subject);
+    }
+    if (typeof value === 'string') {
+        value = parseCsp(value);
+    }
+    expect(normalizeParsedCsp(downgradeCsp2ToCsp1(normalizeParsedCsp(subject))), 'to satisfy', normalizeParsedCsp(value));
+});
+
+describe('downgradeCsp2ToCsp1', () => {
+    it('should leave unsafe-inline', () => {
+        expect("script-src 'unsafe-inline'", 'to come out as', "script-src 'unsafe-inline'");
+    });
+
+    it('should keep unsafe-inline when and drop nonces and hashes', () => {
+        expect("script-src 'sha256-XeYlw2NVzOfB1UCIJqCyGr+0n7bA4fFslFpvKu84IAw=' 'unsafe-inline' 'nonce-foo'", 'to come out as', "script-src 'unsafe-inline'");
+    });
+
+    it('should add unsafe-inline when nonces and hashes are dropped', () => {
+        expect("script-src 'sha256-XeYlw2NVzOfB1UCIJqCyGr+0n7bA4fFslFpvKu84IAw=' 'nonce-foo'", 'to come out as', "script-src 'unsafe-inline'");
+    });
+
+    it('should strip the path from a source expression without a scheme', () => {
+        expect('script-src somewhere.com/with/a/path', 'to come out as', 'script-src somewhere.com');
+    });
+
+    it('should strip the path from a source expression with a scheme', () => {
+        expect('script-src https://somewhere.com/with/a/path', 'to come out as', 'script-src https://somewhere.com');
+    });
+
+    it('should dedupe after stripping the path', () => {
+        expect('script-src somewhere.com/with/a/path somewhere.com/with/another/path', 'to come out as', 'script-src somewhere.com');
+    });
+});
