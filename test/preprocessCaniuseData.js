05c697831f6f2fa227f5de1eaeda300af76f4cd4
Andreas Lind <andreas@one.com>
2017-02-23 22:57:05 +0100

    Gain full coverage.

--- test/preprocessCaniuseData.js
+++ test/preprocessCaniuseData.js
@@ -1,10 +1,10 @@
 const expect = require('unexpected').clone();
 const preprocessCaniuseData = require('../lib/preprocessCaniuseData');
 
-expect.addAssertion('<object> [not] to support <string> <number> <number> <number?>', (expect, subject, caniuseId, major, minor, patch) =>
+expect.addAssertion('<object> [not] to support <string> <number+>', (expect, subject, caniuseId, major, minor, patch) =>
     expect(preprocessCaniuseData(subject)(caniuseId, major, minor, patch), '[not] to be true'));
 
-expect.addAssertion('<object> [not] to be undecided about <string> <number> <number> <number?>', (expect, subject, caniuseId, major, minor, patch) => {
+expect.addAssertion('<object> [not] to be undecided about <string> <number+>', (expect, subject, caniuseId, major, minor, patch) => {
     expect.errorMode = 'nested';
     return expect(preprocessCaniuseData(subject)(caniuseId, major, minor, patch), '[not] to be undefined');
 });
@@ -34,6 +34,12 @@ describe('preprocessCaniuseData', function () {
         expect({ie: { '11': 'n' }}, 'not to support', 'ie', 11, 0);
     });
 
+    it('should let exact major versions through', function () {
+        expect({ie: { '11': 'y' }}, 'to support', 'ie', 11)
+            .and('to support', 'ie', 11, 1)
+            .and('to support', 'ie', 11, 1, 2);
+    });
+
     it('should let exact major.minor versions through', function () {
         expect({ie: { '11.1': 'y' }}, 'to support', 'ie', 11, 1)
             .and('to be undecided about', 'ie', 11, 0);
@@ -58,6 +64,7 @@ describe('preprocessCaniuseData', function () {
     it('should explode version ranges spanning several major and minor versions', function () {
         expect({ie: { '10.3-12.1': 'y' }}, 'to support', 'ie', 10, 3)
             .and('to support', 'ie', 10, 20)
+            .and('to support', 'ie', 10, 20, 4)
             .and('to be undecided about', 'ie', 10, 0)
             .and('to support', 'ie', 11, 0)
             .and('to support', 'ie', 11, 20)
@@ -68,6 +75,7 @@ describe('preprocessCaniuseData', function () {
 
     it('should support multiple non-overlapping ranges', function () {
         expect({ie: { '10.0-10.1': 'n', '10.3-12.1': 'y' }}, 'to support', 'ie', 10, 3)
+            .and('to support', 'ie', 11)
             .and('not to support', 'ie', 10, 0)
             .and('not to support', 'ie', 10, 1)
             .and('to be undecided about', 'ie', 10, 2)
@@ -78,5 +86,11 @@ describe('preprocessCaniuseData', function () {
             .and('to support', 'ie', 12, 0)
             .and('to support', 'ie', 12, 1)
             .and('to be undecided about', 'ie', 12, 2);
+    });
+
+    describe('when there is no data about the browser being queried', function () {
+        it('should return undefined', function () {
+            expect({}, 'to be undecided about', 'ie', 10, 0);
+        });
     });
 });
